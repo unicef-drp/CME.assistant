b54924e3b3127ad7029da2a93532b4604005b202
Yang Liu <yanliu@unicef.org>
2020-01-23 15:01:32 -0500

    [data_helper] revise variable names for ... deaths

--- R/data_helper.R
+++ R/data_helper.R
@@ -4,6 +4,63 @@
 # Yang Liu
 
 
+# Extra -------------------------------------------------------------------
+
+#' a round function that Round off numbers in the conventional way instead of the R round
+#' In R, round(0.5) = 0
+#'
+#' @param x the number
+#' @param digits digits, default to 2
+roundoff <- function(#
+  x, digits = 2
+) {
+  if(!is.numeric(x)) message("x coerse to numeric. ")
+  x <- as.numeric(x)
+  z <- trunc(abs(x)*10^digits + 0.5)
+  z <- sign(x)*z/10^digits
+  return(z)
+}
+
+
+#' a label function to relabel certain variable names
+#'
+#' You can provide a __new_list__ to define the labels. If any label is not
+#' provided, the function will just return the original value
+#'
+#' @param x a element or a vector
+#' @param new_list if you supply a new list the function will use instead of the
+#'   default_labels
+#' @param no_line_break to remove linebreak from the string
+#' @export get.match
+#' @return updated labels as character vector
+get.match <- function(x, new_list = NULL, no_line_break = FALSE){
+  default_labels <- default_label_1
+  if(is.null(new_list)){
+    labs <- default_labels
+  } else {
+    if(is.list(new_list)){
+      labs <- new_list
+    } else {
+      message("new_list must be a list. Still use the default list.")
+      labs <- default_labels
+    }
+  }
+  if(!is.character(x)){
+    message("Coerse input into character.")
+    x <- as.character(x)
+  }
+  out <- rep(NA, length(x))
+  for (i in 1:length(x)){
+    if (is.null(labs[[ x[i] ]])){
+      out[i] <- x[i]
+    }else{
+      out[i] <- labs[[ x[i] ]]
+    }
+  }
+  return(if(no_line_break)gsub("\n", "", out) else out)
+}
+
+
 # Read data ---------------------------------------------------------------
 
 
@@ -28,13 +85,13 @@ get.CME.data <- function(year_range = c(2000:2018), get_what = NULL){
 
   if(is.null(get_what)) {
     CME_types_full <- c("U5MR", "IMR", "NMR")
-    vars_wanted <- c("ISO3Code",	"UNCode",
+    vars_wanted <- c("ISO3Code",	"UNCode", "OfficialName",
                      do.call(paste, expand.grid(CME_types_full, year_range)))
   } else {
-    CME_types_full <- c("Under-five", "Infant", "Neonatal")
+    CME_types_full <- c("Under-five Deaths", "Infant Deaths", "Neonatal Deaths")
     # get all the combination for variable names: e.g. Under-five Deaths 2000, 19*3 = 57 variables
-    vars_wanted <- c("ISO3Code",	"UNCode",
-                     do.call(paste, expand.grid(CME_types_full, "Deaths", year_range)))
+    vars_wanted <- c("ISO3Code",	"UNCode", "OfficialName",
+                     do.call(paste, expand.grid(CME_types_full, year_range)))
   }
   # melt by CME_types_full using `patterns`
   dt_death_long <- data.table::melt(dt[,..vars_wanted],
--- R/plot_helper.R
+++ R/plot_helper.R
@@ -3,44 +3,6 @@
 # 19/10/18
 
 
-#' a label function to relabel certain variable names
-#'
-#' You can provide a __new_list__ to define the labels. If any label is not
-#' provided, the function will just return the original value
-#'
-#' @param x a element or a vector
-#' @param new_list if you supply a new list the function will use instead of the
-#'   default_labels
-#' @param no_line_break to remove linebreak from the string
-#' @export get.label
-#' @return updated labels as character vector
-get.label <- function(x, new_list = NULL, no_line_break = FALSE){
-  default_labels <- default_label_1
-  if(is.null(new_list)){
-    labs <- default_labels
-  } else {
-    if(is.list(new_list)){
-      labs <- new_list
-    } else {
-      message("new_list must be a list. Still use the default list.")
-      labs <- default_labels
-    }
-  }
-  if(!is.character(x)){
-    message("Coerse input into character.")
-    x <- as.character(x)
-  }
-  out <- rep(NA, length(x))
-  for (i in 1:length(x)){
-    if (is.null(labs[[ x[i] ]])){
-      out[i] <- x[i]
-    }else{
-      out[i] <- labs[[ x[i] ]]
-    }
-  }
-  return(if(no_line_break)gsub("\n", "", out) else out)
-}
-
 #' use ggsave to save figs in both png and pdf
 #'
 #' use ggsave to save figs in both png and pdf, default to 600dpi, store in working
